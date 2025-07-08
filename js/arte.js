f91cd50c982c6824715552a267a44b6641759ce7
Monica <monica.lombardi1970@gmail.com>
2025-07-08 14:04:34 -0300

    errorjscuadrocaro

--- js/arte.js
+++ js/arte.js
@@ -10,13 +10,65 @@ class Cuadro {
 //_____________________________________________________
 const cuadroCaro = new Cuadro("cuadroCaro", 10000);
 console.log(cuadroCaro.precio);
+
 const arrayCuadroCaro = Object.values(cuadroCaro);
 console.log(arrayCuadroCaro);
+
+localStorage.setItem("cuadroCaro", JSON.stringify("cuadroCaro"));
+let objetoCaro = JSON.parse(localStorage.getItem("cuadroCaro"));
+console.log(objetoCaro);
+//le declaro con cuanto debe sumarle cada vez que presiona el boton
+/* const MIN_POWER = 10000;
+const MAX_PAWER = 80000;
+if(minCaro === 10000) {
+    console.log("El valor ofrecido es muy bajo, siga participando");
+}
+if(maxCaro === 80000) {
+    console.log("¡Excelente! Decime tu nombre y tu teléfono, y en breve me estaré comunicando con vos.");
+}  */
+let valorCaro = 0; // empieza en 0
+
+const myButtonCaro = document.getElementById("myButtonCaro");
+const precioCaro = document.getElementById("precioCaro");
+
+myButtonCaro.addEventListener("click", () => {
+  if (valorCaro < 70000) {
+    valorCaro += 10000;
+    precioCaro.textContent = `$${valorCaro}`;
+  } else {
+    alert("Llegaste al máximo de $70.000");
+  }
+});
+
+
+
+
+
+
+
+
 //_______________________________________________________
 const cuadroAuto = new Cuadro("cuadroAuto", 50000);
 console.log(cuadroAuto);
 const arrayCuadroAuto = Object.values(cuadroAuto);
 console.log(arrayCuadroAuto);
+//El metodo json convierte auna cadena de caracteres
+//El metodo jsonparse le digo al objeto que lo parse
+localStorage.setItem("cuadroAuto", JSON.stringify("cuadroAuto"));
+let objetoAuto = JSON.parse(localStorage.getItem("cuadroAuto"));
+console.log(objetoAuto);
+const myButtonAuto = document.getElementById("myButtonAuto");
+console.log(myButtonAuto);
+myButtonAuto.addEventListener("click",() =>{
+    alert("click auto")
+})
+
+
+
+
+
+
+
 //__________________________________________________________
 const cuadroVenecia = new Cuadro("cuadroVenecia", 150000);
 console.log(cuadroVenecia);
@@ -38,32 +90,15 @@ let listGroupItemVenecia = document.getElementsByClassName("listGroupItemVenecia
 console.log(listGroupItemVenecia);
 
     
+//_____________________________________________________________________________________________________
 
 
 
-//El metodo json convierte auna cadena de caracteres
-//El metodo jsonparse le digo al objeto que lo parse
-localStorage.setItem("cuadroCaro", JSON.stringify("cuadroCaro"));
-let objetoCaro = JSON.parse(localStorage.getItem("cuadroCaro"));
-
-localStorage.setItem("cuadroAuto", JSON.stringify("cuadroAuto"));
-let objetoAuto = JSON.parse(localStorage.getItem("cuadroAuto"));
 
 localStorage.setItem("cuadroVenecia", JSON.stringify("cuadroVenecia"));
 let objetoVenecia = JSON.parse(localStorage.getItem("cuadroVenecia"));
+
+
 console.log(objetoVenecia);
-console.log(objetoAuto);
-console.log(objetoVenecia);
-
-const myButtonCaro = document.getElementById("myButtonCaro");
-myButtonCaro.addEventListener("click", () => {
-   /*  console.log("click en el boton"); */
-     alert("click en el boton") 
-})
 
 
-const myButtonAuto = document.getElementById("myButtonAuto");
-console.log(myButtonAuto);
-myButtonAuto.addEventListener("click",() =>{
-    alert("click auto")
-})
